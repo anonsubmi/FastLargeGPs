4c8152c678c9709f02b709fc09ec0943e2bd05b4
anonsubmi <123581234+anonsubmi@users.noreply.github.com>
2023-01-25 21:19:39 +0100

    Add files via upload

--- FastOnlineGPsInLargeFields/BathymetryData/runall.m
+++ FastOnlineGPsInLargeFields/BathymetryData/runall.m
@@ -1,3 +1,10 @@
+load('seadepth1.mat');
+load('seadepth2.mat');
+load('seadepth3.mat');
+load('seadepth4.mat');
+seadepth=[seadepth1;seadepth2;seadepth3;seadepth4];
+save('seadepth.mat','seadepth');
+
 ProcessData;
 ProcessDataFewerInducingPoints;
 ProcessDataFewerMeasurements;
